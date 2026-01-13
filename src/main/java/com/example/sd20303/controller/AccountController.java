96aeb7eea1c7ffaad9675e1950a40e890e614bea
nguyenvv4 <nguyenvv4@fpt.edu.vn>
2026-01-13 09:55:15 +0700

    update

--- src/main/java/com/example/sd20303/controller/AccountController.java
+++ src/main/java/com/example/sd20303/controller/AccountController.java
@@ -1,21 +1,32 @@
 package com.example.sd20303.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("account")
 public class AccountController {
+    // mo trang login
     @GetMapping("login")
     public String login() {
         return "home/login.html";
     }
 
+    // thao tac login
     @PostMapping("login")
-    public String login(String name){
-        System.out.println("Da chay vào post");
-        return null;
+    public String login(@RequestParam("username") String un, @RequestParam("password") String pw,
+                        Model model) {
+        System.out.println(un);
+        System.out.println(pw);
+        if (un.equals("nguyenvv4") && pw.equals("123456")) {
+            model.addAttribute("message", "Login thanh cong");
+        } else {
+            model.addAttribute("message", "Login khong thanh cong");
+        }
+        return "home/result.html";
     }
 }
